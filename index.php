98c4f60575b905cd2a1c78dcbb195af989df44cb
alaing <alayo05@gmail.com>
2014-03-17 15:59:54 -0400

    Página em construção

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="/canilbg/resource/ico/dog.ico">
+
+    <title>Canil Blessing of God</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/canilbg/resource/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="/canilbg/resource/css/cover.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+         <div class="inner cover">
+            <img src="resource/img/boxer_400.png" />
+            <h1 class="cover-heading">Canil Blessing of God</h1>            
+            <img src="resource/img/cachorro_fita.png" />                        
+          </div>
+          
+        </div>
+
+      </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="/canilbg/resource/js/jquery-2.0.2.js"></script>
+    <script src="/canilbg/resource/js/bootstrap.min.js"></script>
+    <script src="/canilbg/resource/js/docs.min.js"></script>
+  </body>
+</html>
